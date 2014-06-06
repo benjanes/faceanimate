1f51c26f5272f7c479447e54213ee3e35e005d11
Ben Janes <brjanes@gmail.com>
2014-06-06 17:23:18 -0400

    added a head flip and eye bug combination animation

--- faceanimate.js
+++ faceanimate.js
@@ -1,6 +1,6 @@
 $('document').ready(function() {
 	$('.bigdeal').hover(function() {
-		$('.l_eye').stop().animate(
+		$('#head .l_eye').stop().animate(
 			{
 				width: '750px',
 				height: '807px',
@@ -8,7 +8,7 @@ $('document').ready(function() {
 				left: '-154px'
 			}, 800, 'easeOutElastic'
 		);
-		$('.r_eye').stop().animate(
+		$('#head .r_eye').stop().animate(
 			{
 				width: '750px',
 				height: '807px',
@@ -17,7 +17,7 @@ $('document').ready(function() {
 			}, 800, 'easeOutElastic'
 		);
 	}, function() {
-		$('.l_eye').stop().animate(
+		$('#head .l_eye').stop().animate(
 			{
 				width: '250px',
 				height: '269px',
@@ -25,7 +25,7 @@ $('document').ready(function() {
 				left: '96px'	
 			}, 800
 		);
-		$('.r_eye').stop().animate(
+		$('#head .r_eye').stop().animate(
 			{
 				width: '250px',
 				height: '269px',
@@ -36,7 +36,7 @@ $('document').ready(function() {
 	}); //end hover
 	
 	
-	$('.flip').hover( function(){
+	$('#headflip .flip').hover( function(){
 		$('#headflip .face').stop().animate(
 			{
 				left: '465px'
@@ -64,4 +64,104 @@ $('document').ready(function() {
 		});
 		
 	}); //end other hover
+	
+	$('#flipANDbug .flip').hover( function(){
+		$('#flipANDbug .face').stop().animate(
+			{
+				left: '515px'
+			}, 50
+		).css({
+	        '-moz-transform': 'scaleX(-1)',
+	        '-o-transform': 'scaleX(-1)',
+	        '-webkit-transform' : 'scaleX(-1)',
+	        'transform' : 'scaleX(-1)',
+	        'filter' : 'FlipH',
+	        '-ms-filter' : 'FlipH'
+		});
+		$('#flipANDbug .l_eye').stop().animate(
+			{
+				left: '420px'
+			}, 50
+		).css({
+	        '-moz-transform': 'scaleX(-1)',
+	        '-o-transform': 'scaleX(-1)',
+	        '-webkit-transform' : 'scaleX(-1)',
+	        'transform' : 'scaleX(-1)',
+	        'filter' : 'FlipH',
+	        '-ms-filter' : 'FlipH'
+		}).delay(150).animate(
+			{
+				width: '750px',
+				height: '807px',
+				top: '-271px',
+				left: '170px'
+			}, 1200, 'easeOutElastic'
+		);
+		$('#flipANDbug .r_eye').stop().animate(
+			{
+				left: '469px'
+			}, 50
+		).css({
+	        '-moz-transform': 'scaleX(-1)',
+	        '-o-transform': 'scaleX(-1)',
+	        '-webkit-transform' : 'scaleX(-1)',
+	        'transform' : 'scaleX(-1)',
+	        'filter' : 'FlipH',
+	        '-ms-filter' : 'FlipH'
+		}).delay(150).animate(
+			{
+				width: '750px',
+				height: '807px',
+				top: '-298px',
+				left: '219px'
+			}, 1200, 'easeOutElastic'
+		);			
+	}, function(){
+		$('#flipANDbug .face').stop().animate(
+			{
+				left: '0px'
+			}, 50
+		).css({
+	        '-moz-transform': '',
+	        '-o-transform': '',
+	        '-webkit-transform' : '',
+	        'transform' : '',
+	        'filter' : '',
+	        '-ms-filter' : ''
+		});
+		$('#flipANDbug .l_eye').stop().css({
+	        '-moz-transform': '',
+	        '-o-transform': '',
+	        '-webkit-transform' : '',
+	        'transform' : '',
+	        'filter' : '',
+	        '-ms-filter' : ''
+		}).animate(
+			{
+				width: '250px',
+				height: '269px',
+				top: '-2px',
+				left: '96px'
+			}, 50
+		);
+		$('#flipANDbug .r_eye').stop().css({
+	        '-moz-transform': '',
+	        '-o-transform': '',
+	        '-webkit-transform' : '',
+	        'transform' : '',
+	        'filter' : '',
+	        '-ms-filter' : ''
+		}).animate(
+			{
+				width: '250px',
+				height: '269px',
+				top: '-29px',
+				left: '46px'
+			}, 50
+		);
+	}
+
+
+
+); //end hover
 }); //end ready
